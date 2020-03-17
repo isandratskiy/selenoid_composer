36d933f343f9e2d53a8e0cb4136d7342a2e4b062
isandratskiy <i.sandrackii@gmail.com>
2020-03-17 20:57:35 +0200

    added makefile

---
+++ Makefile
@@ -0,0 +1,7 @@
+start:
+	\
+	docker pull selenoid/vnc:chrome_80.0 && \
+	docker pull selenoid/vnc:firefox_74.0 && \
+	docker-compose up -d --force-recreate
+stop:
+	docker-compose down --rmi all
--- selenoid_start.sh
+++ selenoid_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "==============================> Pulling Chrome" 
+echo " ==============================> pulling Chrome  " 
 docker pull selenoid/vnc_chrome:80.0
-echo "==============================> Pulling Firefox"
+echo " ==============================> pulling Firefox "
 docker pull selenoid/vnc_firefox:72.0
-echo "Starting Selenoid Service ... "
-docker-compose up --force-recreate
+echo "starting Selenoid service ...................... "
+docker-compose up -d --force-recreate
