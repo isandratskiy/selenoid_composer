382de641a7e79049594c4de851b0d352d9544661
isandratskiy <i.sandrackii@gmail.com>
2020-04-04 04:06:41 +0300

    cleanup

--- selenoid_start.sh
+++ selenoid_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo " ==============================> pulling Chrome  "
-docker pull selenoid/vnc_chrome:80.0
+docker pull selenoid/vnc:chrome_80.0
 echo " ==============================> pulling Firefox "
-docker pull selenoid/vnc_firefox:72.0
+docker pull selenoid/vnc:firefox_74.0
 echo "starting Selenoid service ...................... "
 docker-compose up -d --force-recreate
